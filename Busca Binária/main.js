0e8ebc82fffa3fdd9bf14ed35219aaa0e82f49d9
João Vítor S. F <53799823+jjoaovitor7-unit@users.noreply.github.com>
2020-09-15 21:20:48 -0300

    Create main.js

---
+++ Busca Binária/main.js
@@ -0,0 +1,8 @@
+const bubbleSort = require('./bubbleSort');
+const buscaBinaria = require('./buscaBinaria');
+
+let arr = [85, 65, 14, 57, 31, 76, 92];
+bS = bubbleSort(arr);
+element = 57
+
+console.log(buscaBinaria(57, bS));
